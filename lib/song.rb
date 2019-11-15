7355961dfab794802077bc8aede030e54a675443
Makeda P Mckune <makedamckune@gmail.com>
2019-11-15 02:15:11 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+class Song
+  
+attr_accessor :name, :artists, :genre
+
+  def initialize(name,artist,genre)
+    @name = name
+    @artists = artist
+    @genre = genre
+  end 
+  def
+  end 
+
+end 
